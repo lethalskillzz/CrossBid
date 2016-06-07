0896343abc1c4b39303b83c3d1667cf83168717e
Ibrahim Amin <lethalskillzz@gmail.com>
2016-06-07 15:36:01 +0100

    initialise git

---
+++ app/src/main/java/com/crossover/crossbid/helper/DBHelper.java
@@ -0,0 +1,22 @@
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "crossbid.db";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME , null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // TODO Auto-generated method stub
+        db.execSQL(
+                "create table contacts " +
+                        "(id integer primary key, name text,phone text,email text, street text,place text)"
+        );
+    }
+
+}

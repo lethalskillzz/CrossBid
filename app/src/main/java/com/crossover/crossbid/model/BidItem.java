532f7de9aba90fc2ae6475988e16c1bfd7bd8d48
Ibrahim Amin <lethalskillzz@gmail.com>
2016-06-10 07:28:50 +0100

    implent ItemHolder

--- app/src/main/java/com/crossover/crossbid/model/BidItem.java
+++ app/src/main/java/com/crossover/crossbid/model/BidItem.java
@@ -5,4 +5,69 @@ package com.crossover.crossbid.model;
  */
 public class BidItem {
 
+    private int id;
+    private byte[] image;
+    private String name;
+    private float price;
+    private boolean isBid;
+
+    public BidItem() {
+    }
+
+    public BidItem(int id, byte[] image, String name, float price, boolean isBid) {
+
+        super();
+        this.id = id;
+        this.image = image;
+        this.name = name;
+        this.price = price;
+        this.isBid = isBid;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+
+    public Boolean getIsBid() {
+        return isBid;
+    }
+
+    public void setIsBid(Boolean isBid) {
+        this.isBid = isBid;
+    }
+
+
 }
